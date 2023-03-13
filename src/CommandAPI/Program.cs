a088aafd80f68dc57891b375874c0d086ae5f36f
Pallab Das <pallabdas04@outlook.com>
2023-03-13 02:08:14 -0500

    Initial Commit

---
+++ src/CommandAPI/Program.cs
@@ -0,0 +1,26 @@
+namespace CommandAPI
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            //Register services to enable the use of controllers throughout our application
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            //app.MapGet("/", () => "Hello World!");
+            
+            app.Run();
+        }
+    }
+}
